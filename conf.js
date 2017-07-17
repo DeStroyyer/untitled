3fef29f5d008fe48dac65797132aa9d0344769d6
tvoloshenko <tvoloshenko@iMac-12.local>
2017-07-17 13:42:33 +0300

    first commit

---
+++ conf.js
@@ -0,0 +1,9 @@
+/**
+ * Created by tvoloshenko on 7/17/17.
+ */
+// conf.js
+exports.config = {
+    framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['tests/**/*.spec.js']
+}
---
+++ spec.js
@@ -0,0 +1,11 @@
+/**
+ * Created by tvoloshenko on 7/17/17.
+ */
+// spec.js
+describe('Protractor Demo App', function() {
+    it('should have a title', function() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+
+        expect(browser.getTitle()).toEqual('Super Calculator');
+    });
+});
